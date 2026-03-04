6a960f097dd153b9d2315793d04f65294d4f3141
Trackcodex <quantaforge25@gmail.com>
2026-03-04 17:25:44 +0530

    infra: commit core terraform configuration for AWS setup

---
+++ terraform/alb.tf
@@ -0,0 +1,34 @@
+resource "aws_alb" "main" {
+  name            = "${var.project_name}-alb"
+  subnets         = aws_subnet.public.*.id
+  security_groups = [aws_security_group.lb.id]
+}
+
+resource "aws_alb_target_group" "app" {
+  name        = "${var.project_name}-tg"
+  port        = 4000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = "/health"
+    unhealthy_threshold = "2"
+  }
+}
+
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = aws_alb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.app.id
+    type             = "forward"
+  }
+}
